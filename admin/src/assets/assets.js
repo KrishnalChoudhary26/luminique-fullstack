3d937eb75e56f7f3b4ea8cbeb6a7d21c2bcaa71d
KrishnalChoudhary26 <krishnalc2612@gamil.com>
2026-03-04 00:27:31 +0530

    fixed minor syntax error

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -3,7 +3,7 @@ import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.svg'
-import logo2 from './logo2.PNG'
+import logo2 from './logo2.png'
 
 export const assets = {
     logo,
